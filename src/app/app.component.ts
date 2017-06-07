fb88d7d547967b94a2f227d31c5981437f3ca6e0
Gilles Vivies <gilles.vivies@gmail.com>
2017-06-07 23:36:08 +0200

    Refactoring and analogic clock css styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
-import { style, animate, AnimationBuilder } from '@angular/animations';
+import { style, animate, AnimationBuilder, AnimationPlayer } from '@angular/animations';
 
 const rotatingSecond = { transform: 'rotate( {{ seconds }} )' };
 
@@ -16,48 +16,93 @@ export class AppComponent implements OnInit {
   private hours: number;
   private minutes: number;
   private seconds: number;
+  private oldHours: number;
+  private oldMins: number;
 
   constructor(private _builder: AnimationBuilder) {
 
   }
 
   ngOnInit() {
+
     const secondArrow = document.getElementById('secondArrow');
     const minArrow = document.getElementById('minArrow');
     const hourArrow = document.getElementById('hourArrow');
 
-    Observable.interval(500)
+    const cardHours = document.getElementById('cardHours');
+    const cardMins = document.getElementById('cardMins');
+    const cardSeconds = document.getElementById('cardSeconds');
+
+    const cardHoursAnimationPlayer = this.buildCard(cardHours);
+    const cardMinsAnimationPlayer = this.buildCard(cardMins);
+    const cardSecondsAnimationPlayer = this.buildCard(cardSeconds);
+
+    this.currentDate = new Date();
+    this.oldMins = this.currentDate.getMinutes();
+    this.oldHours = this.currentDate.getHours();
+
+    let secondAnimationPlayer;
+    let minuteAnimationPlayer;
+    let hourAnimationPlayer;
+
+    Observable.interval(1000)
       .subscribe(() => {
         this.currentDate = new Date();
         this.seconds = this.currentDate.getSeconds();
         this.minutes = this.currentDate.getMinutes();
         this.hours = this.currentDate.getHours();
-
-        const secondAnimationPlayer = this.buildSecondsOrMinsAnimationBuilder(false, this.seconds, secondArrow);
-        const minuteAnimationPlayer = this.buildSecondsOrMinsAnimationBuilder(false, this.minutes, minArrow);
-        const hourAnimationPlayer = this.buildSecondsOrMinsAnimationBuilder(true, this.hours, hourArrow);
-
+        secondAnimationPlayer = this.buildSecondsOrMinsAnimationBuilder(false, this.seconds, secondArrow);
+        minuteAnimationPlayer = this.buildSecondsOrMinsAnimationBuilder(false, this.minutes, minArrow);
+        hourAnimationPlayer = this.buildSecondsOrMinsAnimationBuilder(true, this.hours, hourArrow);
         secondAnimationPlayer.play();
         minuteAnimationPlayer.play();
         hourAnimationPlayer.play();
+
+        // We animate the hours card only when hour has changed
+        if (this.hours !== this.oldHours) {
+          this.oldHours = this.hours;
+          cardHoursAnimationPlayer.play();
+        }
+
+        // We animate the minutes card only when minute has changed
+        if (this.minutes !== this.oldMins) {
+          this.oldMins = this.minutes;
+          cardMinsAnimationPlayer.play();
+        }
+
+        // We animate the seconds card each time
+        cardSecondsAnimationPlayer.play();
       });
   }
 
-  buildSecondsOrMinsAnimationBuilder(isHours: boolean, nbUnits: number, element: any) {
-    const angle = isHours ? this.calcRotationForHours(nbUnits) : this.calcRotationForMinutesSeconds(nbUnits);
+  // This animation player calculates the rotation angle according to the number
+  // of units (hours, minutes or seconds)
+  buildSecondsOrMinsAnimationBuilder(isHours: boolean, nbUnits: number, element: any): AnimationPlayer {
+    const angle = isHours ? this.calcRotationAngleForHours(nbUnits) : this.calcRotationAngleForMinutesSeconds(nbUnits);
     const builder = this._builder.build([
       style({ transform: 'rotate( ' + angle + 'deg )' }),
-      animate(1000, style({ transform: 'rotate( ' + angle + 'deg )' }))
+      animate('1000ms cubic-bezier(.17,.67,.88,.1)', style({ transform: 'rotate( ' + angle + 'deg )' }))
     ]);
     return builder.create(element);
   }
 
-  calcRotationForMinutesSeconds(sOrM: number): number {
+  buildCard(element: any): AnimationPlayer {
+    const builder = this._builder.build([
+      style({ height: '0', color: '#d2d2d2' }),
+      animate('150ms ease', style({ height: '72px', color: '#000' }))
+    ]);
+    return builder.create(element);
+  }
+
+  calcRotationAngleForMinutesSeconds(sOrM: number): number {
     return (sOrM / 60) * 360;
   }
 
-  calcRotationForHours(h: number): number {
+  calcRotationAngleForHours(h: number): number {
     return (h / 12) * 360;
   }
 
+  formatDigit(digit: number): string {
+    return (digit < 10) ? '0' + digit : '' + digit;
+  }
 }
